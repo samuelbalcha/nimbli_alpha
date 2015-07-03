f6cf40ec927ef584b8aacec06156e142e2f95e40
Samuel <samuel.balcha@gmail.com>
2015-07-03 11:59:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    TOKEN_SECRET: process.env.TOKEN_SECRET || 'Plato4ndP14typuSW41kInT0aBar',
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/nimbli_alpha',
-    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook Client Secret',
-    FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Client Secret',
-    GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
-    LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
-    WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret',
-    TWITTER_KEY: process.env.TWITTER_KEY || 'Twitter Consumer Key',
-    TWITTER_SECRET: process.env.TWITTER_SECRET || 'Twitter Consumer Secret',
-    TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'Twitter Callback URL',
-    YAHOO_SECRET: process.env.YAHOO_SECRET || 'Yahoo Client Secret',
-    AWS_KEYID : process.env.AWS_KEYID || 'AKIAJOP6CMSQJTNSAAEQ',
-    AWS_SECRET : process.env.AWS_SECRET || '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',
-    AWS_BUCKET : process.env.AWS_BUCKET || 'samppa',
-    AWS_ACL : process.env.AWS_ACL || 'public-read'
+    TOKEN_SECRET: process.env.TOKEN_SECRET || 'stuff',
++    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/nalpha',
++    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'stuff',
++    GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'stuff',
++    AWS_KEYID : process.env.AWS_KEYID || 'stuff',
++    AWS_SECRET : process.env.AWS_SECRET || 'stuff',
++    AWS_BUCKET : process.env.AWS_BUCKET || 'stuff',
+     AWS_ACL : process.env.AWS_ACL || 'public-read'
 };
